a02b2244ca35a8c8c7519bf905726e7dbce44da0
hayashihiroaki <61njn0@gmail.com>
2020-09-18 14:36:08 +0900

    既読機能のフロントエンド実装

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -7,8 +7,9 @@ class PostsController < ApplicationController
     Post.create(content: params[:content])
     redirect_to action: :index
   end
-  
+
   def checked
+  
     post = Post.find(params[:id])
     if post.checked 
       post.update(checked: false)
